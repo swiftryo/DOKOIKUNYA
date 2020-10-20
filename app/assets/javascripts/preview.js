1d42492b7cbd828ca7c2ac79afb650f738234c5f
vagrant <vagrant@localhost.localdomain>
2020-10-20 08:33:04 +0000

    Add_Preview

---
+++ app/assets/javascripts/preview.js
@@ -0,0 +1,12 @@
+$(function(){
+    // inputのidから情報の取得
+    $('#product_image').on('change', function (e) {
+// ここから既存の画像のurlの取得
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $(".image").attr('src', e.target.result);
+    }
+// ここまで
+    reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
+});
+});
